c262c572f5cc827e56cc4f2ff3bdf5c6bd5bb06e
brynsgtn <bryansuguitan17@gmail.com>
2025-03-22 07:47:24 -0500

    Configure routes to server

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import authRoutes from '../backend/routes/authRoutes.js'
 
 import { connectDB } from './db/connectDB.js';
 
@@ -11,6 +12,7 @@ app.get('/', (req, res) => {
     res.send("Intern Attendance Web App");
 });
 
+app.use('/api/auth', authRoutes)
 
 app.listen(3000, () => {
     connectDB();
