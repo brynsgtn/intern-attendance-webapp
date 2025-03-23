0b0192a58da419f78ab6fa921c14989457e4f0bb
brynsgtn <bryansuguitan17@gmail.com>
2025-03-23 10:29:17 -0500

    Implemented login functionality

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -89,7 +89,39 @@ export const verifyEmail = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-    res.send("Login");
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ success: false, message: "Invalid credentials" });
+        };
+        const isPasswordValid = await bcryptjs.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(400).json({ success: false, message: "Invalid credentials" });
+        };
+
+        if (!user.isVerified) {
+            return res.status(400).json({ success: false, message: "User not verified" });
+        };
+
+        generateTokenAndSetCookie(res, user._id);
+
+        user.lastLogin = new Date();
+        await user.save();
+
+        res.status(200).json({
+            success: true,
+            message: "Logged in successfully",
+            user: {
+                ...user._doc,
+                password: undefined,
+            },
+        });
+    } catch (error) {
+        console.log("error in verifyEmail", error);
+        res.status(500).json({ success: false, message: "Server error" });
+    }
 };
 
 export const logout = async (req, res) => {
