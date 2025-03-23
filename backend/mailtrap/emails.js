942e5efdbe8873a637f20cff72d5c2e033b42120
brynsgtn <bryansuguitan17@gmail.com>
2025-03-23 10:56:19 -0500

    Imported PASSWORD_RESET_REQUEST_TEMPLATE

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -1,4 +1,4 @@
-import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js";
+import { VERIFICATION_EMAIL_TEMPLATE, PASSWORD_RESET_REQUEST_TEMPLATE } from "./emailTemplates.js";
 import { mailtrapClient, sender } from "./mailtrapConfig.js";
 
 
