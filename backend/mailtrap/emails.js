b6c103a1b70fc6a4e4ca1fd67017f20aa221f4ae
brynsgtn <bryansuguitan17@gmail.com>
2025-03-23 08:10:30 -0500

    Added sendWelcomeEmail functionality

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -19,3 +19,29 @@ export const sendVerificationEmail = async (email, verificationToken) => {
         throw new Error(`Error sending verification email: ${error}`);
     };
 };
+
+
+export const sendWelcomeEmail = async (email, name) => {
+    const recipient = [{ email }];
+
+    try {
+
+        const response = await mailtrapClient.send({
+            from: sender,
+            to: recipient,
+            template_uuid: "1bcb4426-dfd3-46e7-93a1-00c60f6fe595",
+            template_variables: {
+                "company_info_name": "Ollopa Corporation",
+                "name": name,
+                "company_info_address": "No. 22 Ipil St Sitio Seville",
+                "company_info_city": "Quezon City",
+                "company_info_zip_code": "1100",
+                "company_info_country": "Philippines"
+            }
+        });
+        console.log("Welcome email sent successfully", response);
+    } catch (error) {
+        console.error(`Error sending welcome email`, error);
+        throw new Error(`Error sending welcome email: ${error}`);
+    }
+}
