77a3749bad465deb626ede34cd9c11ce7a182ed6
brynsgtn <bryansuguitan17@gmail.com>
2025-03-23 23:11:40 -0500

    Added isTeamLeader

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -52,6 +52,10 @@ const userSchema = new mongoose.Schema({
         type: Boolean,
         default: false,
     },
+    isTeamLeader: {
+        type: Boolean,
+        default: false,
+    },
     isAdmin: {
         type: Boolean,
         default: false,
