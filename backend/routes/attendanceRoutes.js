4f87062678a6b15821785b2f3d85666a2fbea682
brynsgtn <bryansuguitan17@gmail.com>
2025-03-25 04:25:46 -0500

    Added get-all-attendance, update-time-in/:userId, update-time-out/:userId, approve-attendance/:userId, reject-attendance/:userId

--- backend/routes/attendanceRoutes.js
+++ backend/routes/attendanceRoutes.js
@@ -1,12 +1,25 @@
 import express from "express";
 
-import { getUserAttendance, timeIn, timeOut } from "../controllers/attendanceControllers.js";
+import { 
+    getUserAttendance, 
+    timeIn, 
+    timeOut,
+    updateTimeIn,
+    updateTimeOut,
+    approveAttendance,
+    rejectAttendance,
+    getAllAttendance
+} from "../controllers/attendanceControllers.js";
 
 const router = express.Router();
 
-
+router.get('/get-attendance/:userId', getUserAttendance);
+router.get('/get-all-attendance', getAllAttendance);
 router.post('/time-in', timeIn);
 router.post('/time-out', timeOut);
-router.get('/get-attendance/:userId', getUserAttendance);
+router.patch('/update-time-in/:userId', updateTimeIn); 
+router.patch('/update-time-out/:userId', updateTimeOut);
+router.post('/approve-attendance/:userId', approveAttendance);
+router.post('/reject-attendance/:userId', rejectAttendance);
 
 export default router; 
